b5419730d9b90bd5a654da3c59b5357da2d96873
Oleg <senyuholeg@gmail.com>
2017-08-21 19:24:07 +0200

    first

---
+++ src/main/java/com/TestController.java
@@ -0,0 +1,27 @@
+package com;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+
+public class TestController {
+    @RequestMapping(value = "/test")
+    public ModelAndView test(){
+        ModelAndView modelAndView = new ModelAndView("test");
+
+
+
+
+        modelAndView.addObject("name", "Oleg");
+
+        return  modelAndView;
+
+
+
+
+    }
+
+}
